0eee9ae4788233f3cd6010c335c58780a2a3d458
Deepak <deepak_808@yahoo.com>
2021-08-09 15:12:24 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+const FirstComponent = (props) => {
+    return(
+        <div className="App">
+            <h4>App components</h4>
+        </div>
+
+    )
+
+    };
+//arrow components also may have props validation
+export default FirstComponent;
